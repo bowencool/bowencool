dafc02f0fd3bb7a5291b3be5d2ec4a02f534bad5
博文 <zhaobowen@mockuai.com>
2020-09-17 12:37:28 +0800

    upd

--- src/file.js
+++ src/file.js
@@ -6,39 +6,71 @@
  * @returns {Promise<FileList>}
  */
 export const selectFile = ({ accept = 'image/*', multiple = false } = {}) =>
-  new Promise(resolve => {
+  new Promise((resolve) => {
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = accept;
     if (multiple) input.multiple = multiple;
-    input.onchange = e => {
-      resolve(e.target.files);
+    input.onchange = (e) => {
+      const { files } = e.target;
+      input.remove();
+      resolve(files);
     };
+    document.body.appendChild(input);
     input.click();
   });
 
+export function getBlobByUrl(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
+
+    xhr.responseType = 'blob';
+
+    xhr.onload = () => {
+      const blob = new Blob([xhr.response], { type: 'application/octet-stream' });
+      resolve(blob);
+    };
+    xhr.onerror = reject;
+    xhr.send();
+  });
+}
+let t = 0; // 防死循环
+export async function downloadByUrl(url, filename) {
+  t++;
+  const a = document.createElement('a');
+  if (t > 10) return;
+  // 跨域不能重命名
+  if (filename && url.indexOf(window.location.origin) === -1) {
+    console.log('跨域下载 && 重命名');
+    downloadBlob(await getBlobByUrl(url), filename);
+    return;
+  }
+  a.href = url;
+  if (filename) a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  t = 0;
+}
 /**
  * @description 将文件保存到本地
  * @param {Blob} blob Blob对象
  * @param {string} [filename] 要保存的文件名
  */
-export const downloadFile = (blob, filename = blob.filename) => {
-  const a = document.createElement('a');
+export async function downloadBlob(blob, filename = blob.filename) {
   const blobUrl = window.URL.createObjectURL(blob);
-  a.href = blobUrl;
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
+  await downloadByUrl(blobUrl, filename);
   window.URL.revokeObjectURL(blobUrl);
-  a.remove();
-};
+}
 
 /**
  * @description 读取图片文件原始宽高
  * @param {Blob} file
  * @returns {Promise<{width:number,height:number}>}
  */
-export const getImgRect = file =>
+export const getImgRect = (file) =>
   new Promise((resolve, reject) => {
     const imgDom = document.createElement('img');
     const src = window.URL.createObjectURL(file);
@@ -61,7 +93,7 @@ export function blobToDataURL(file) {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
+    reader.onerror = reject;
   });
 }
 /**
